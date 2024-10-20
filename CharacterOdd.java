89d91031636dd06ede5cae92538663bf5b42ed45
Geetika <139126957+Geetika2708@users.noreply.github.com>
2024-10-20 23:13:09 +0530

    Create CharacterOdd.java

---
+++ CharacterOdd.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class CharacterOdd{
+  public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    String str = sc.nextLine();
+    
+    for(int i =0; i<str.length();i++){
+      if(i%2!=0){
+       System.out.print(str.charAt(i));
+      }
+    }
+
+  }
+}
